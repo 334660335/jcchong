8d37e2c13ea0d638ab79a479c2722f19976cde1d
wangrenjie_pc <wangrenjie@jcchong.com>
2017-04-01 23:43:51 +0800

    change verify

--- app/Http/Controllers/Wechat/WechatBaseController.php
+++ app/Http/Controllers/Wechat/WechatBaseController.php
@@ -46,7 +46,8 @@ class WechatBaseController extends BaseController
         $server->setMessageHandler(function(){
             return "您好！欢迎关注 overtrue!";
         });
-        return $app->server->serve();
+        $server->serve()->send();
+        //return $app->server->serve();
     }
 
 }
