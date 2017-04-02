98b54f6a6a26090468ca2d8dcacfd781733487b2
wangrenjie_pc <wangrenjie@jcchong.com>
2017-04-02 10:12:19 +0800

    change

--- app/Http/Controllers/Wechat/WechatBaseController.php
+++ app/Http/Controllers/Wechat/WechatBaseController.php
@@ -14,7 +14,7 @@ class WechatBaseController extends BaseController
     public function __construct()
     {
         parent::__construct();
-        //$this->__initwechat();
+        $this->__initwechat();
     }
 
     /**
